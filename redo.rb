c7d846d1c5223b9d0e4d83b4533ef09d67791bbf
James Kelly <james@hicag.org>
2026-03-04 18:51:33 -0600

    Move Run class under Redo

--- redo.rb
+++ redo.rb
@@ -2,21 +2,21 @@
 
 require 'open3'
 
-# Run a system command
-class Run
-  def self.command(name, command, *args)
-    args.flatten!
-
-    out, error, status = Open3.capture3(command, *args)
-    return [out, error, status] if status.success?
-
-    raise ["#{name} failed:", command, *args, error].join("\n")
-  end
-end
-
 # Run a redo command
 class Redo
   def self.ifchange(*targets)
     Run.command('Redo command', 'redo-ifchange', targets)
+  end
+
+  # Run a system command
+  class Run
+    def self.command(name, command, *args)
+      args.flatten!
+
+      out, error, status = Open3.capture3(command, *args)
+      return [out, error, status] if status.success?
+
+      raise ["#{name} failed:", command, *args, error].join("\n")
+    end
   end
 end
